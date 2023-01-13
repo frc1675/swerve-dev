03f91b93bc3ff5ae1d3bcb59decf732b30090170
python-42 <herrmann.jake@protonmail.com>
2023-01-13 01:13:46 -0600

    Added scaling control to robot speed and each axis Scaler values are provided by 4 shuffleboard widgets

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,6 +1,12 @@
 package frc.robot;
 
+import java.util.Map;
+
+import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.Button;
@@ -13,17 +19,38 @@ public class RobotContainer {
 
   private final XboxController m_controller = new XboxController(Constants.DRIVER_CONTROLLER);
 
+  private final ShuffleboardTab tab = Shuffleboard.getTab("Drivetrain");
+
+  private NetworkTableEntry scalerSlider = tab.add("Speed scaler", 1)
+  .withWidget(BuiltInWidgets.kNumberSlider)
+  .withProperties(Map.of("min", 0.1, "max", 1, "block increment", 0.1))
+  .getEntry();
+
+  private NetworkTableEntry leftXScaler = tab.add("X Translation Scaler", 1)
+  .withWidget(BuiltInWidgets.kNumberSlider)
+  .withProperties(Map.of("min", 0, "max", 1, "block increment", 0.1))
+  .getEntry();
+  
+  private NetworkTableEntry leftYScaler = tab.add("Y Translation Scaler", 1)
+  .withWidget(BuiltInWidgets.kNumberSlider)
+  .withProperties(Map.of("min", 0, "max", 1, "block increment", 0.1))
+  .getEntry();
+
+  private NetworkTableEntry rightXScaler = tab.add("Rotation Scaler", 1)
+  .withWidget(BuiltInWidgets.kNumberSlider)
+  .withProperties(Map.of("min", 0, "max", 1, "block increment", 0.1))
+  .getEntry();
+
   public RobotContainer() {
-    // Set up the default command for the drivetrain.
     // The controls are for field-oriented driving:
     // Left stick Y axis -> forward and backwards movement
     // Left stick X axis -> left and right movement
     // Right stick X axis -> rotation
     m_drivetrainSubsystem.setDefaultCommand(new DefaultDriveCommand(
             m_drivetrainSubsystem,
-            () -> -modifyAxis(m_controller.getLeftY()) * DrivetrainSubsystem.MAX_VELOCITY_METERS_PER_SECOND,
-            () -> -modifyAxis(m_controller.getLeftX()) * DrivetrainSubsystem.MAX_VELOCITY_METERS_PER_SECOND,
-            () -> -modifyAxis(m_controller.getRightX()) * DrivetrainSubsystem.MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND
+            () -> -modifyAxis(m_controller.getLeftY()) * DrivetrainSubsystem.MAX_VELOCITY_METERS_PER_SECOND * leftYScaler.getDouble(1.0),
+            () -> -modifyAxis(m_controller.getLeftX()) * DrivetrainSubsystem.MAX_VELOCITY_METERS_PER_SECOND * leftXScaler.getDouble(1.0),
+            () -> -modifyAxis(m_controller.getRightX()) * DrivetrainSubsystem.MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND * rightXScaler.getDouble(1.0)
     ));
 
     configureButtonBindings();
@@ -54,10 +81,14 @@ public class RobotContainer {
     }
   }
 
-  private static double modifyAxis(double value) {
+  private double modifyAxis(double value) {
     value = deadband(value, 0.05);
 
     value = Math.copySign(value * value, value);
+
+    double scaler = scalerSlider.getDouble(1.0);
+
+    value = value * scaler; //scale down value to slow robot
 
     return value;
   }
